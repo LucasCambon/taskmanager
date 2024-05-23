0d42a1d543fd6c45606235ccd771a7d11b288604
LucasCambon <lucambon08@gmail.com>
2024-05-23 06:54:54 -0300

    validation tests terminadas y fix en las test de user para evitar fallos innecesario

---
+++ tests/spec/middlewares/validationMiddleware.spec.js
@@ -0,0 +1,132 @@
+const request = require('supertest');
+const app = require('../../../src/app');
+const db = require('../../../src/database/models'); 
+
+
+describe('Validation Middleware', () => {
+
+    let token;
+
+    // Set up the database before running the tests
+    beforeAll(async () => {
+        await db.sequelize.sync({ force: true });
+        const user = await request(app)
+            .post('/api/users/register')
+            .send({
+                username: 'testuserValidationMiddleware',
+                email: 'testuserValidationMiddleware@example.com',
+                password: 'Test@1234',
+                phoneNumber: '+14579514657'
+            })
+        // Simulate logging in to get a token
+        const response = await request(app)
+            .post('/api/users/login')
+            .send({
+                username: 'testuserValidationMiddleware',
+                password: 'Test@1234'
+            });
+
+        token = response.body.token;
+    });
+    // Clean up tasks after each test
+    afterEach(async () => {
+        await db.Task.destroy({ where: {} });
+        await db.User.destroy({ where: {} });
+    });
+
+    // Close the database connection after all tests are done
+    afterAll(async () => {
+        await db.sequelize.close();
+    });
+
+    test('should return 400 if username is missing or invalid in registration', async () => {
+        const response = await request(app)
+            .post('/api/users/register')
+            .send({
+                email: 'user@example.com',
+                password: 'Password123!',
+                phoneNumber: '+154598724'
+            });
+        expect(response.status).toBe(400);
+        expect(response.body.errors).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ msg: 'Username must be a string.' }),
+                expect.objectContaining({ msg: 'Username must be at least 3 characters long.' })
+            ])
+        );
+    });
+
+    test('should return 400 if email is invalid in registration', async () => {
+        const response = await request(app)
+            .post('/api/users/register')
+            .send({
+                username: 'username',
+                email: 'invalid-email',
+                password: 'Password123!',
+                phoneNumber: '+154598724'
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.body.errors).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ msg: 'Email must be valid.' })
+            ])
+        );
+    });
+
+    test('should return 400 if password is weak in registration', async () => {
+        const response = await request(app)
+            .post('/api/users/register')
+            .send({
+                username: 'username',
+                email: 'user@example.com',
+                password: 'weakpass',
+                phoneNumber: '+154598724'
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.body.errors).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ msg: 'Password must be between 8-16 characters, include at least one uppercase letter, one lowercase letter, one number, and one special character.' })
+            ])
+        );
+    });
+
+    test('should return 400 if title is missing or invalid in task creation', async () => {
+        const response = await request(app)
+            .post('/api/tasks/create')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                description: 'Task description',
+                dueDate: '2024-05-31',
+                status: 'pending'
+            })
+            .expect('Content-Type', /json/)
+            .expect(400);   
+
+        expect(response.body.errors).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ msg: 'Title must be a string.' }),
+                expect.objectContaining({ msg: 'Title must be at least 3 characters long.' })
+            ])
+        );
+    });
+
+    test('should return 200 if valid task is created', async () => {
+        const taskData = {
+            title: 'Valid Task Title',
+            description: 'Task description',
+            dueDate: '2024-05-31',
+            status: 'pending'
+        }
+        const response = await request(app)
+            .post('/api/tasks/create')
+            .set('Authorization', `Bearer ${token}`)
+            .send(taskData)
+            .expect('Content-Type', /json/)
+            .expect(201);
+            expect(response.body).toHaveProperty('id');
+            expect(response.body.title).toBe(taskData.title);
+    });
+
+});
--- tests/spec/models/users.spec.js
+++ tests/spec/models/users.spec.js
@@ -2,6 +2,20 @@ const db = require('../../../src/database/models');
 
 describe('User Model', () => {
 
+    beforeAll(async () => {
+      await db.sequelize.sync({ force: true });
+    });
+
+    // Clean up tasks after each test
+    afterEach(async () => {
+      await db.User.destroy({ where: {} }); 
+    });
+
+    // Close the database connection after all tests are done
+    afterAll(async () => {
+      await db.sequelize.close();
+    });
+
     test('should create a valid user', async () => {
       const userData = {
         username: 'testuser',
@@ -43,18 +57,31 @@ describe('User Model', () => {
 
     test('should update user data', async () => {
         const existingUsername = 'testuser'; // Assuming this user exists
+        const userData = {
+          username: 'testuserUpdate',
+          email: 'testuserUpdate@example.com',
+          password: 'StrongPassword123@',
+          phoneNumber: "+1547972344"
+        };
+        const newUser = await db.User.create(userData);
         const updatedData = {
             email: 'newemail@example.com',
             // Other fields you want to update
         };
-        const [updatedRowsCount, updatedUsers] = await db.User.update(updatedData, { where: { username: existingUsername }, returning: true });
+        const [updatedRowsCount, updatedUsers] = await db.User.update(updatedData, { where: { username: newUser.username }, returning: true });
         expect(updatedRowsCount).toBe(1); // Ensure that one user was updated
         expect(updatedUsers.length).toBe(1); // Ensure that one updated user is returned
     });
 
     test('should delete a user', async () => {
-        const existingUsername = 'testuser'; // Assuming this user exists
-        const deletedRowsCount = await db.User.destroy({ where: { username: existingUsername } });
+        const userData = {
+          username: 'testuserDelete',
+          email: 'testuserDelete@example.com',
+          password: 'StrongPassword123@',
+          phoneNumber: "+1547972344"
+        };
+        const newUser = await db.User.create(userData);
+        const deletedRowsCount = await db.User.destroy({ where: { username: newUser.username } });
         expect(deletedRowsCount).toBe(1); // Ensure that one user was deleted
     });
 });

49e948eec2036a33982738516c2077905df0d85e
LucasCambon <lucambon08@gmail.com>
2024-05-25 07:54:47 -0300

    update para un 100% en las pruebas de index model

--- tests/spec/models/index.spec.js
+++ tests/spec/models/index.spec.js
@@ -1,18 +1,60 @@
-const index = require('../../../src/database/models/index');
 const Sequelize = require('sequelize');
-// File created by sequelize, so I created this unit test only for practice.
-describe('Index', () => {
-  it('should import models correctly', () => {
-    expect(index).toHaveProperty('User');
-    expect(index).toHaveProperty('Task');
-  });
+const path = require('path');
+require('dotenv').config({ path: '.env' });
 
-  it('should set up Sequelize correctly', () => {
-    expect(index.sequelize).toBeInstanceOf(Sequelize.Sequelize);
-  });
+describe('Database Initialization', () => {
+    let originalEnv;
 
-  it('should establish associations between models if necessary', () => {
-    expect(index.User).toHaveProperty('hasMany');
-    expect(index.User.associations).toHaveProperty('Tasks');
-  });
+    beforeAll(() => {
+        // Save the original state of environment variables
+        originalEnv = { ...process.env };
+    });
+
+    afterAll(() => {
+        // Restore the original state of environment variables
+        process.env = originalEnv;
+    });
+
+    beforeEach(() => {
+        jest.resetModules(); // Clear the module cache before each test
+    });
+
+    test('should create Sequelize connection using environment variable', () => {
+        process.env.NODE_ENV = 'development';
+        // Mock the configuration to use the environment variable
+        jest.mock('../../../src/database/config/database.js', () => ({
+            development: {
+                username: process.env.DB_USER,
+                password: process.env.DB_PASS,
+                database: process.env.DB_DATABASE,
+                host: process.env.DB_HOST,
+                dialect: 'postgres',
+                port: process.env.DB_PORT,
+                use_env_variable: 'DB_CONNECTION',
+            },
+        }));
+
+        const db = require('../../../src/database/models'); // Import the module after setting the environment variable
+
+        expect(db.sequelize).toBeDefined();
+    });
+
+    test('should create Sequelize connection using database config', () => {
+
+        // Mock the configuration to use the database config directly
+        jest.mock('../../../src/database/config/database.js', () => ({
+            development: {
+                username: 'postgres',
+                password: 'admin',
+                database: 'task_manager',
+                host: '127.0.0.1',
+                dialect: 'postgres'
+            }
+        }));
+
+        const db = require('../../../src/database/models'); // Import the module after clearing the environment variable
+
+        expect(db.sequelize).toBeDefined();
+    });
 });
+
